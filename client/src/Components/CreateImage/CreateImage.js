27f9df5de6e258c89af8dbb74a43ebcbcd5871ef
durgeshmeena <durgeshkumar@iitg.ac.in>
2022-07-12 16:02:21 +0530

    updated validation for CreateImage URI

--- client/src/Components/CreateImage/CreateImage.js
+++ client/src/Components/CreateImage/CreateImage.js
@@ -1,14 +1,21 @@
 function CreateImage (product)  {
-    // console.log(product);
-    const imgData = product.image.data.data;
-    const base64ImageString = Buffer.from(imgData, 'binary').toString('base64')
-    // console.log(base64ImageString);
-    const imgSrc = `data:${product.image.contentType};base64,${base64ImageString}`;
+    // console.log(product,  );
+
+    if(product.image === undefined){
+		return ""
+	}
+
+	const imgData = product.image.data.data;
+	const base64ImageString = Buffer.from(imgData, 'binary').toString('base64')
+	// console.log(base64ImageString);
+	const imgSrc = `data:${product.image.contentType};base64,${base64ImageString}`;
+	return imgSrc;
+	
 
     // const imgWidth = {
     //   // width: "20rem",
     // }
-    return imgSrc;  
+      
     // return <img src={imgSrc} alt="product" className="img-fluid" width="200rem"  />
   }
 
