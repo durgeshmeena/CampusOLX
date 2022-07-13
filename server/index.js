246c322680101ede9ccba30db4c3fb12e74d0a2a
durgeshmeena <durgeshkumar@iitg.ac.in>
2022-07-13 14:14:17 +0530

    updating server index for deployment

--- server/index.js
+++ server/index.js
@@ -16,9 +16,7 @@ app.use(router);
 
 // servinging react app
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+
 
 InitiateMongoServer()
 .then(()=>{
@@ -29,6 +27,10 @@ InitiateMongoServer()
     app.use('/admin', routerAdminBro);
   }
     
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
